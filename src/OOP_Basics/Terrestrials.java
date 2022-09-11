91d6fc4fc094c5945c97eea760c43141f8574804
Miguel Gonzales <88881293+mgrmars@users.noreply.github.com>
2022-09-11 17:12:32 -0500

    Update Terrestrials.java

--- src/OOP_Basics/Terrestrials.java
+++ src/OOP_Basics/Terrestrials.java
@@ -1,9 +1,10 @@
 package OOP_Basics;
 
 // La clase la cuadrupeda hereda los atributos y métodos de la clase animales
+// por lo tanto los datos atributos de animales y sus métodos estan disponibles en esta clase (clase Terrestres).
 public class Terrestrials extends Animals {
 
     public void run() {
-        // ...
+        System.out.println(this.name + " is running");
     }
 }
